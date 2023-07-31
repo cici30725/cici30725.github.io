9e190065623425b0e5c9e693d0e315b038ebcfb6
Kang Hsu <popkart13@gmail.com>
2023-07-31 22:22:12 +0800

    Fixed homepage and added contacts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,25 +5,35 @@
 
 <div class="flex flex-col items-center mt-10 py-5 gap-y-5">
     <div class="whitespace-nowrap">
-        <h1 class="text-3xl font-bold">HSI-KANG HSU</h1>
+        <h1 class="text-4xl font-bold">HSI-KANG HSU</h1>
+    </div>
+    <div class="font-serif">
+        <div>
+            Email: <a href="mailto:hsuhsikang@gmail.com" class="underline underline-offset-2">hsuhsikang@gmail.com</a>
+        </div>
+        <div>
+            Github: <a href="https://github.com/cici30725" class="underline underline-offset-2">github.com/cici30725</a>
+        </div>
     </div>
     <div class="max-w-xl">
         <p>
-            I recently graduated with a Master's degree in Computer Science from National Taiwan University.
+            I recently graduated from National Taiwan University with a master's degree in computer science.
             I wrote my thesis on efficient methods for pricing interest rate derivatives with the LIBOR Market Model (LMM).
         </p>
     </div>
 </div>
 
-<section class="p-12 ml-40">
-    <h1 class="text-xl font-bold my-4">Notes</h1>
-    <WritingList data={writing.notes} />
-</section>
+<div class="flex flex-col items-start m-auto max-w-4xl">
+    <div class="p-12">
+        <h1 class="text-xl font-bold my-4">Notes</h1>
+        <WritingList data={writing.notes} />
+    </div>
 
-<section class="p-12 ml-40">
-    <h1 class="text-xl font-bold my-4">Academic Writing</h1>
-    <WritingList data={writing.papers} />
-</section>
+    <div class="p-12">
+        <h1 class="text-xl font-bold my-4">Academic Writing</h1>
+        <WritingList data={writing.papers} />
+    </div>
+</div>
  
 <style lang="postcss">
     :global(html) {
